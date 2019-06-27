0f2585e63c15719d4aafae5c7b2d5a26cb525900
Kazuhiro Oinuma <oinume@gmail.com>
2019-06-27 11:04:44 +0900

    Implement -short, -last feature

--- cmd/path-shrinker/main.go
+++ cmd/path-shrinker/main.go
@@ -17,7 +17,9 @@ $ shrink_path
 /Use/k/g/s/gi/oi/pa
 */
 var (
-	tilde = flag.Bool("tilde", false, " Substitute ~ for the home directory.")
+	short = flag.Bool("short", false, "Truncate directory names to the first character. Without -short, names are truncated without making them ambiguous.")
+	tilde = flag.Bool("tilde", false, "Substitute ~ for the home directory.")
+	last  = flag.Bool("last", false, "Print the last directory's full name.")
 )
 
 func main() {
@@ -42,14 +44,8 @@ func run(args []string) (string, error) {
 		path = p
 	}
 
-	transformers := make([]shrinker.Transformer, 0, 10)
-	if *tilde {
-		transformers = append(transformers, &shrinker.TildeTransformer{
-			HomeDir: os.Getenv("HOME"), // TODO: go-homedir
-		})
-	}
 	dirs := strings.Split(path, string(os.PathSeparator))
-
+	transformers := createTransformers(dirs)
 	shrink, err := executeTransform(transformers, dirs)
 	if err != nil {
 		return "", err
@@ -57,9 +53,34 @@ func run(args []string) (string, error) {
 	return shrink, nil
 }
 
+func createTransformers(dirs []string) []shrinker.Transformer {
+	// -tilde, -short, -last are enabled
+	// -> Process order: tilde, short, last
+	// -amb, -last are enabled
+	// -last just override last element with original value
+	transformers := make([]shrinker.Transformer, 0, 4)
+	if *tilde {
+		transformers = append(transformers, &shrinker.TildeTransformer{
+			HomeDir: os.Getenv("HOME"), // TODO: go-homedir
+		})
+	}
+	if *short {
+		transformers = append(transformers, &shrinker.ShortTransformer{})
+	}
+	if *last {
+		transformers = append(transformers, &shrinker.PreserveLastTransformer{
+			Last: dirs[len(dirs)-1],
+		})
+	}
+	return transformers
+}
+
 func executeTransform(transformers []shrinker.Transformer, input []string) (string, error) {
 	result := input
 	for _, t := range transformers {
+		if len(result) == 0 {
+			return "", fmt.Errorf("empty result was returned")
+		}
 		output, err := t.Transform(result)
 		if err != nil {
 			return "", err
--- transformer.go
+++ transformer.go
@@ -5,6 +5,12 @@ import (
 	"strings"
 )
 
+type Config struct {
+	PreserveLast bool
+	Shorten      bool
+	ReplaceTilde bool
+}
+
 type Transformer interface {
 	Transform(input []string) ([]string, error)
 }
@@ -17,4 +23,27 @@ func (tt *TildeTransformer) Transform(input []string) ([]string, error) {
 	path := strings.Join(input, string(os.PathSeparator))
 	path = strings.Replace(path, tt.HomeDir, "~", -1)
 	return strings.Split(path, string(os.PathSeparator)), nil
+}
+
+type ShortTransformer struct{}
+
+func (st *ShortTransformer) Transform(input []string) ([]string, error) {
+	length := len(input)
+	result := make([]string, 0, length)
+	for _, v := range input {
+		result = append(result, string([]rune(v)[0]))
+	}
+	return result, nil
+}
+
+type PreserveLastTransformer struct {
+	Last string
+}
+
+func (plt *PreserveLastTransformer) Transform(input []string) ([]string, error) {
+	result := make([]string, len(input))
+	copy(result, input)
+	length := len(input)
+	result[length-1] = plt.Last
+	return result, nil
 }
