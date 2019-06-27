dc3aa486fe0e269e74e596babafce1daa05c5273
Kazuhiro Oinuma <oinume@gmail.com>
2019-06-27 21:55:12 +0900

    Add more unit tests

--- cmd/path-shrinker/main_test.go
+++ cmd/path-shrinker/main_test.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	shrinker "github.com/oinume/path-shrinker"
 )
 
 func TestRun(t *testing.T) {
+	os.Setenv("HOME", "/home/oinume")
+
 	tests := map[string]struct {
 		path   string
 		config *shrinker.Config
 		want   string
 	}{
-		"simple": {
+		"short": {
 			path: "/home/oinume/go",
 			config: &shrinker.Config{
 				Mode: shrinker.ModeShort,
 			},
 			want: "/h/o/g",
+		},
+		"tilde short": {
+			path: "/home/oinume/go",
+			config: &shrinker.Config{
+				Mode:         shrinker.ModeShort,
+				ReplaceTilde: true,
+			},
+			want: "~/g",
+		},
+		"tilde short last": {
+			path: "/home/oinume/go/src",
+			config: &shrinker.Config{
+				Mode:         shrinker.ModeShort,
+				ReplaceTilde: true,
+				PreserveLast: true,
+			},
+			want: "~/g/src",
 		},
 	}
 
