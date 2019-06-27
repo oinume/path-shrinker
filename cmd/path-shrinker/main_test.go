d77d7628fe5a74a677df2a98e8dd488871330dc4
Kazuhiro Oinuma <oinume@gmail.com>
2019-06-27 21:49:58 +0900

    Add unit tests for run

---
+++ cmd/path-shrinker/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	shrinker "github.com/oinume/path-shrinker"
+)
+
+func TestRun(t *testing.T) {
+	tests := map[string]struct {
+		path   string
+		config *shrinker.Config
+		want   string
+	}{
+		"simple": {
+			path: "/hoge",
+			config: &shrinker.Config{
+				Mode: shrinker.ModeShort,
+			},
+			want: "/h",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := run([]string{test.path}, test.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got != test.want {
+				t.Errorf("got %q but want %q", got, test.want)
+			}
+		})
+	}
+}
